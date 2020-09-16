9c188690986389475c6b3affb2ce7f29f71d8ad2
Ole Jørgen Knoph <s354371@oslomet.no>
2020-09-16 22:39:50 +0200

    OPPGAVER KLASSER OG OBJEKTER UKE 38

---
+++ src/OppgaverKlasserObjekter.java
@@ -0,0 +1,30 @@
+public class OppgaverKlasserObjekter {
+/*Oppgave 1 (&2)
+    static class Boker {
+        public String tittel;
+        public int pris;
+        public String forfatter;
+        public int iSBN_nummer;
+
+        public String utskrift() {
+            System.out.print(tittel+" " + pris + " "+ forfatter + " "+ iSBN_nummer);
+            return "";
+        }
+
+    }
+
+    public static void main (String [] args) {
+        Boker nyBok = new Boker();
+        nyBok.tittel = "Ole";
+        nyBok.pris = 500;
+        nyBok.forfatter = "OJK";
+        nyBok.iSBN_nummer = 1234;
+
+        System.out.print(nyBok.utskrift());
+    }
+    */
+/*Oppgave 2*/
+    public static void main (String [] args) {
+
+    }
+}
