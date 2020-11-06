68b596f07673fd8d6debe5205e62a1180a874b05
Ole Jørgen Knoph <s354371@oslomet.no>
2020-11-06 18:51:55 +0100

    Gjort responsiv under 885px. For 100% mangler meny sideBar og responsiv under 500px (100vw hvert element)

--- SeeCorrectMake/Nettsiden/src/index.js
+++ SeeCorrectMake/Nettsiden/src/index.js
@@ -5,31 +5,39 @@
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
-    console.log("1");
-    if (document.body.scrollLeft > 5 || document.documentElement.scrollLeft > 5) {
-        /*legger til klassen sidebarcollapse og logocollapse som har andre egenskaper i css*/
-        document.getElementsByClassName("sideBar")[0].classList.add("sideBarCollapse");
-        document.getElementsByClassName("logo")[0].classList.add("logoCollapse");
 
-/*Fjerner all tekst slik at kun ikoner står igjen*/
-        document.getElementsByClassName("slagOrd")[0].innerHTML = "";
-        document.getElementsByClassName("fas fa-info")[0].innerHTML = "";
-        document.getElementsByClassName("fas fa-phone")[0].innerHTML = "";
-        document.getElementsByClassName("fas fa-paper-plane")[0].innerHTML = "";
-        document.getElementsByClassName("fas fa-bars")[0].innerHTML = "";
-    }
-    else if (document.body.scrollLeft < 5 || document.documentElement.scrollLeft < 5) {
-/*fjerner klassene som ble lagt til*/
-        document.getElementsByClassName("sideBar")[0].classList.remove("sideBarCollapse");
-        document.getElementsByClassName("logo")[0].classList.remove("logoCollapse");
+if (window.innerWidth > 885) {
 
-/*Legger tilbake originale verdier*/
-        document.getElementsByClassName("slagOrd")[0].innerHTML = "LEARN.<br>EXPERIENCE.<br>INNOVATE.<br>";
-        document.getElementsByClassName("fas fa-info")[0].innerHTML = "REQUEST INFO";
-        document.getElementsByClassName("fas fa-phone")[0].innerHTML = "(800)658-5744";
-        document.getElementsByClassName("fas fa-paper-plane")[0].innerHTML = "APPLY AT UAT";
-        document.getElementsByClassName("fas fa-bars")[0].innerHTML = "MENU";
+        if (document.body.scrollLeft > 5 || document.documentElement.scrollLeft > 5) {
+            /*legger til klassen sidebarcollapse og logocollapse som har andre egenskaper i css*/
+            document.getElementsByClassName("sideBar")[0].classList.add("sideBarCollapse");
+            document.getElementsByClassName("logo")[0].classList.add("logoCollapse");
+            console.log(window.screen.width);
+
+                /*Fjerner all tekst slik at kun ikoner står igjen*/
+                document.getElementsByClassName("slagOrd")[0].innerHTML = "";
+                document.getElementsByClassName("fas fa-info")[0].innerHTML = "";
+                document.getElementsByClassName("fas fa-phone")[0].innerHTML = "";
+                document.getElementsByClassName("fas fa-paper-plane")[0].innerHTML = "";
+                document.getElementsByClassName("fas fa-bars")[0].innerHTML = "";
+        }
+
+        else if (document.body.scrollLeft < 5 || document.documentElement.scrollLeft < 5) {
+
+            /*fjerner klassene som ble lagt til*/
+            document.getElementsByClassName("sideBar")[0].classList.remove("sideBarCollapse");
+            document.getElementsByClassName("logo")[0].classList.remove("logoCollapse");
+            console.log(window.screen.width);
+
+            /*Legger tilbake originale verdier*/
+            document.getElementsByClassName("slagOrd")[0].innerHTML = "LEARN.<br>EXPERIENCE.<br>INNOVATE.<br>";
+            document.getElementsByClassName("fas fa-info")[0].innerHTML = "REQUEST INFO";
+            document.getElementsByClassName("fas fa-phone")[0].innerHTML = "(800)658-5744";
+            document.getElementsByClassName("fas fa-paper-plane")[0].innerHTML = "APPLY AT UAT";
+            document.getElementsByClassName("fas fa-bars")[0].innerHTML = "MENU";
+            }
     }
+
 }
 
 /*-----------------------------------------------------------------------------------------*/
